0d67726af282743491ada035c1c76390beca72eb
Amirul Hossain <amirul1709@hotmail.com>
2025-03-17 16:40:50 -0600

    creating movie review page

--- movie-site-frontend/movie.js
+++ movie-site-frontend/movie.js
@@ -1,8 +1,4 @@
-const API_KEY = ""
-
 const API_LINK = `https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}&page=1`
-const IMG_PATH = `https://image.tmdb.org/t/p/w1280`
-const SEARCH_API = `https://api.themoviedb.org/3/search/movie?&api_key=${API_KEY}&query=`
 
 const main = document.getElementById("section")
 const form = document.getElementById("form")
