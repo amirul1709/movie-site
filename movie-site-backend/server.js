907863b029484c993504a69fe83afd4642629085
Amirul Hossain <amirul1709@hotmail.com>
2025-03-18 18:43:57 -0600

    connected backend to frontend

--- movie-site-backend/server.js
+++ movie-site-backend/server.js
@@ -4,11 +4,16 @@ import reviews from "./api/reviews.route.js"
 
 const app = express()
 
-app.use(cors())
+const corsOptions = {
+    origin: '*',
+    methods: 'GET,POST,PUT,DELETE,OPTIONS',
+    allowedHeaders: 'Content-Type,Authorization'
+}
+
+app.use(cors(corsOptions))
 app.use(express.json())
 
 app.use("/api/v1/reviews", reviews)
 app.use("*", (req, res) => res.status(404).json({ error: "not found" }))
-
 
 export default app
--- movie-site-frontend/movie.js
+++ movie-site-frontend/movie.js
@@ -1,4 +1,4 @@
-const API_LINK = `https://localhost:8000/api/v1/reviews/`
+const API_LINK = `http://localhost:8000/api/v1/reviews/`
 
 const url = new URL(location.href)
 const movieId = url.searchParams.get("id")
@@ -12,24 +12,33 @@ title.innerText = movieTitle
 returnReviews(API_LINK)
 
 function returnReviews(url) {
-    fetch(url + "movie/" + movieId).then(res => res.json())
+    fetch(url + "movie/" + movieId)
+        .then(res => {
+            if (!res.ok) {
+                throw new Error(`HTTP error! status: ${res.status}`);
+            }
+            return res.json();
+        })
         .then(function (data) {
             data.forEach(review => {
                 const div_card = document.createElement("div")
 
                 div_card.innerHTML = `
-                <div class="row">
-                    <div class="column">
-                        <div class="card" id="${review._id}">
-                            <p><strong>Review: </strong>${review.review}</p>
-                            <p><strong>User: </strong>${review.user}</p>
-                            <p><a href="#" onclick="editReview('${review._id}', '${review.review}', '${review.user}')">✏️</a> <a href="#" onclick="deleteReview('${review._id}')">🗑️</a></p>
+                    <div class="row">
+                        <div class="column">
+                            <div class="card" id="${review._id}">
+                                <p><strong>Review: </strong>${review.review}</p>
+                                <p><strong>User: </strong>${review.user}</p>
+                                <p><a href="#" onclick="editReview('${review._id}', '${review.review}', '${review.user}')">✏️</a> <a href="#" onclick="deleteReview('${review._id}')">🗑️</a></p>
+                            </div>
                         </div>
                     </div>
-                </div>
-                `
+                    `
 
-                main.appendChild(div_row)
+                main.appendChild(div_card)
             })
         })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+        });
 }
